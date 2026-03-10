ad4654d2d1aa06c9db7da6488204663c96e698e6
nbozinovic23 <nemanjabozinovic90@gmail.com>
2026-03-10 21:05:12 +0100

    Add card layout on home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToyModel } from '../../models/toy.model';
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  toys = signal<ToyModel[]>([])
 
+  constructor() {
+    // API poziv dolazi kasnije kada dodamo ToyService
+  }
 }
